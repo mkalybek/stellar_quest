6ca9bcee1f7b677612ecda928dc1fadba967ddda
railanbaigazy <baigazy.railan@gmail.com>
2024-10-05 12:36:30 +0500

    frontend header start

---
+++ frontend/src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import './header.scss'
+
+const Header = () => {
+    return (
+        <header className='anton-regular'>
+            Header
+        </header>
+    );
+};
+
+export default Header;
