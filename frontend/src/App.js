621e97072bc727690569df3b6a73b9b7dd7518c1
railanbaigazy <baigazy.railan@gmail.com>
2024-10-05 18:12:55 +0500

    simulator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Header from './components/header/header';
 import Exoplanets from './sections/exoplanets/exoplanets';
+import Simulator from './sections/simulator/simulator';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Exoplanets />
+      <Simulator />
     </div>
   );
 }
