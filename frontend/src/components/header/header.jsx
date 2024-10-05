df985e6677527e39ae72d652c3f415bd7c6a31d8
Uakks <sopygaliu@mail.ru>
2024-10-05 13:45:26 +0500

    Raw header done

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -2,8 +2,17 @@ import './header.scss'
 
 const Header = () => {
     return (
-        <header className='anton-regular'>
-            Header
+        <header className='header anton-regular'>
+            <ul>
+                <li><a>HISTORY</a></li>
+                <li><a>METHODS OF SEARCH</a></li>
+                <li><a>TYPES AND CLASSES</a></li>
+                <li><a>EXOPLANETS</a></li>
+                <li><a>SIMULATOR</a></li>
+                <li><a>MAPS</a></li>
+                <li><a>EXOMOONS</a></li>
+                <li><a>EXPLORE</a></li>
+            </ul>
         </header>
     );
 };
