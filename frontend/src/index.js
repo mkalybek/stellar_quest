20ef0be99ed48dc83290a56a4c0bbcdb5729ac50
x1m1x <ximizivanov@gmail.com>
2024-10-06 15:49:51 +0500

    added basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,43 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainPage from './pages/mainPage';
-import SimulatorGasGiantPage from './pages/simulatorGasGiantPage';
-import SimulatorEarthTypePage from './pages/simulatorEarthTypePage';
-import ExoplanetPage from './pages/exoplanetPage';
-import GeneratePage from './pages/generatePage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./pages/mainPage";
+import SimulatorGasGiantPage from "./pages/simulatorGasGiantPage";
+import SimulatorEarthTypePage from "./pages/simulatorEarthTypePage";
+import ExoplanetPage from "./pages/exoplanetPage";
+import GeneratePage from "./pages/generatePage";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <MainPage />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: '/simulator/gasgiant',
-    element: <SimulatorGasGiantPage />,
-  },
-  {
-    path: '/simulator/earthtype',
-    element: <SimulatorEarthTypePage />,
-  },
-  {
-    path: '/exoplanet/:planetName/:textureImage',
-    element: <ExoplanetPage />,
-  },
-  {
-    path: '/generate/:planetName/:planetType/:imageName/:cloudsCount',
-    element: <GeneratePage />,
-  },
-]);
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <MainPage />,
+            errorElement: <div>404 Not Found</div>,
+        },
+        {
+            path: "/simulator/gasgiant",
+            element: <SimulatorGasGiantPage />,
+        },
+        {
+            path: "/simulator/earthtype",
+            element: <SimulatorEarthTypePage />,
+        },
+        {
+            path: "/exoplanet/:planetName/:textureImage",
+            element: <ExoplanetPage />,
+        },
+        {
+            path: "/generate/:planetName/:planetType/:imageName/:cloudsCount",
+            element: <GeneratePage />,
+        },
+    ],
+    {
+        basename: "/stellar-quest",
+    }
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
