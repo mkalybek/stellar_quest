e7b69e8804e553c363d4c4b6324450f2a0c5b099
x1m1x <ximizivanov@gmail.com>
2024-10-06 16:16:45 +0500

    change header links

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -4,14 +4,14 @@ const Header = () => {
     return (
         <header className='header anton-regular'>
             <ul>
-                <li><a href="/stellar-quest#history">HISTORY</a></li>
-                <li><a href="/stellar-quest#methods">METHODS OF SEARCH</a></li>
-                <li><a href="/stellar-quest#types">TYPES AND CLASSES</a></li>
-                <li><a href="/stellar-quest#exoplanets">EXOPLANETS</a></li>
-                <li><a href="/stellar-quest#simulator">SIMULATOR</a></li>
-                <li><a href="/stellar-quest#maps">MAPPING</a></li>
-                <li><a href="/stellar-quest#exomoons">EXOMOONS</a></li>
-                <li><a href="/stellar-quest#">EXPLORE</a></li>
+                <li><a href="/stellar-quest/#history">HISTORY</a></li>
+                <li><a href="/stellar-quest/#methods">METHODS OF SEARCH</a></li>
+                <li><a href="/stellar-quest/#types">TYPES AND CLASSES</a></li>
+                <li><a href="/stellar-quest/#exoplanets">EXOPLANETS</a></li>
+                <li><a href="/stellar-quest/#simulator">SIMULATOR</a></li>
+                <li><a href="/stellar-quest/#maps">MAPPING</a></li>
+                <li><a href="/stellar-quest/#exomoons">EXOMOONS</a></li>
+                <li><a href="/stellar-quest/nasagame/#">EXPLORE</a></li>
             </ul>
         </header>
     );
